4c068dcdbb0c50cefaf55504b92447ee66e774f2
Your Name <rodrigoe923@gmail.com>
2020-11-24 19:28:04 -0300

    commit1

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
 window.addEventListener('load', initial);
 
 function initial(){
-
     let input = document.querySelector('.container input');
     input.addEventListener('input', captureEvent);
-} 
+}
 
 function captureEvent(event){
     let input = event.target.value;
-
+    let inputExtenso = document.querySelector('#extenso');
     addnumeric(input);
-    extenso(input);
+
+    inputExtenso.value = extenso(input) 
 }
 
 function addnumeric(input){
@@ -18,301 +18,112 @@ function addnumeric(input){
     numeric.value = input;
 }
 
-function extenso(input){
-    let inputValue = input.toString();
-    let resultado = null;
-
-    if(inputValue > 100){
-        resultado = calculo(inputValue);
+function extenso(value){
+    let resultado;
+    if(value > 99){
+        resultado = numeros(value)
+        if(value[1] + value[2] < 20){
+            resultado = resultado + menorVinte(value[1] + value[2])           
+        }      
     }else{
-        resultado = calculoMenor(inputValue)
+        if(value > 9){
+            if(value < 20){
+                resultado = menorVinte(value) 
+            }else{
+                resultado = numeros(value)
+            }
+        }else{
+            resultado = unidade(value)
+        }   
     }
-
-    let extenso = document.querySelector("#extenso")
-    extenso.value = resultado;
-
+    return resultado
 }
 
-function dezen (inputValue, resultado){
-    let cent = inputValue.slice(1, -1);
-
-    if(cent == 9) resultado = resultado + 'noventa';  
-    if(cent == 8) resultado = resultado + 'oitenta'; 
-    if(cent == 7) resultado = resultado + 'setenta';
-    if(cent == 6) resultado = resultado + 'sessenta';    
-    if(cent == 5) resultado = resultado + 'cinquenta';    
-    if(cent == 4) resultado = resultado + 'quarenta';    
-    if(cent == 3) resultado = resultado + 'trinta';    
-    if(cent == 2) resultado = resultado + 'vinte';    
-    return resultado;
-}
-
-function unidade(inputValue, resultado){   
-    let un = inputValue.slice(2);
-
-    if(un == 9) resultado = resultado + ' e nove';
-    if(un == 8) resultado = resultado + ' e oito';
-    if(un == 7) resultado = resultado + ' e sete';
-    if(un == 6) resultado = resultado + ' e seis';  
-    if(un == 5) resultado = resultado + ' e cinco'; 
-    if(un == 4) resultado = resultado + ' e quatro';
-    if(un == 3) resultado = resultado + ' e trez';   
-    if(un == 2) resultado = resultado + ' e dois';
-    if(un == 1) resultado = resultado + ' e um'; 
-    return resultado;
-}
-
-function unidadeMenor(inputValue, resultado){   
-    let un = inputValue.slice(1);
-
-    if(un == 9) resultado = resultado + ' e nove';
-    if(un == 8) resultado = resultado + ' e oito';
-    if(un == 7) resultado = resultado + ' e sete';
-    if(un == 6) resultado = resultado + ' e seis';  
-    if(un == 5) resultado = resultado + ' e cinco'; 
-    if(un == 4) resultado = resultado + ' e quatro';
-    if(un == 3) resultado = resultado + ' e trez';   
-    if(un == 2) resultado = resultado + ' e dois';
-    if(un == 1) resultado = resultado + ' e um'; 
-    return resultado;
-}
-
-function espeCif(inputValue, resultado){   
-    let especif = inputValue.slice(1);
-
-    if(especif == 19) resultado = resultado + ' desenove';
-    if(especif == 18) resultado = resultado + ' dessoito';
-    if(especif == 17) resultado = resultado + ' desessete';
-    if(especif == 16) resultado = resultado + ' desesseis';  
-    if(especif == 15) resultado = resultado + ' quinze'; 
-    if(especif == 14) resultado = resultado + ' quatorze';
-    if(especif == 13) resultado = resultado + ' treze';   
-    if(especif == 12) resultado = resultado + ' doze';
-    if(especif == 11) resultado = resultado + ' onze';
-    if(especif == 10) resultado = resultado + ' dez';
-    if(especif == 9) resultado = resultado + ' nove';
-    if(especif == 8) resultado = resultado + ' oito';
-    if(especif == 7) resultado = resultado + ' sete';
-    if(especif == 6) resultado = resultado + ' seis';  
-    if(especif == 5) resultado = resultado + ' cinco'; 
-    if(especif == 4) resultado = resultado + ' quatro';
-    if(especif == 3) resultado = resultado + ' trez';   
-    if(especif == 2) resultado = resultado + ' dois';
-    if(especif == 1) resultado = resultado + ' um'; 
-    return resultado;
-}
-
-function dezenMenor (inputValue, resultado){
-    let cent = inputValue.slice(0, 1);
-
-    if(cent == 9) resultado = resultado + 'noventa';  
-    if(cent == 8) resultado = resultado + 'oitenta'; 
-    if(cent == 7) resultado = resultado + 'setenta';
-    if(cent == 6) resultado = resultado + 'sessenta';    
-    if(cent == 5) resultado = resultado + 'cinquenta';    
-    if(cent == 4) resultado = resultado + 'quarenta';    
-    if(cent == 3) resultado = resultado + 'trinta';    
-    if(cent == 2) resultado = resultado + 'vinte';
-    if(cent == 1) resultado = resultado + 'dez';    
-    return resultado;
-}
-
-function menorVinte(inputValue){
-    if(inputValue.slice(1) < 20){
-
-        let final = espeCif(inputValue, resultado);
-        resultado = final
-        return resultado
-    }else{
-        let resultUni = unidade(inputValue, resulDeze)
-        resultado = resultUni 
-        return resultado
-    }
-}
-
-function espeCifTeste(inputValue){   
-    let especif = inputValue;
-    let resultado = " ";
-    if(especif == 19) resultado = ' desenove';
-    if(especif == 18) resultado = ' dessoito';
-    if(especif == 17) resultado = ' desessete';
-    if(especif == 16) resultado = ' desesseis';  
-    if(especif == 15) resultado = ' quinze'; 
-    if(especif == 14) resultado = ' quatorze';
-    if(especif == 13) resultado = ' treze';   
-    if(especif == 12) resultado = ' doze';
-    if(especif == 11) resultado = ' onze';
-    if(especif == 10) resultado = ' dez';
-    if(especif == 9) resultado = ' nove';
-    if(especif == 8) resultado =  'oito';
-    if(especif == 7) resultado = ' sete';
-    if(especif == 6) resultado = ' seis';  
-    if(especif == 5) resultado = ' cinco'; 
-    if(especif == 4) resultado = ' quatro';
-    if(especif == 3) resultado = ' trez';   
-    if(especif == 2) resultado = ' dois';
-    if(especif == 1) resultado = ' um'; 
-    return resultado;
-}
-
-function menorVinteTeste(inputValue){
-    let final = espeCifTeste(inputValue, resultado);
-    resultado = final
-    return resultado  
-}
-
-function calculoMenor(inputValue){
-    let dez = inputValue.slice(0)
+function numeros(value){
     let resultado = '';
-    if(dez >= 20){
-        resultado = dezenMenor(inputValue, resultado);
-        numeric = unidadeMenor(inputValue, resultado)
-        return numeric
-
-    }else{
-        resutado = menorVinteTeste(inputValue);
-        return resutado
-    }  
+    for(let i = 0; i < value.length; i++){
+        if(i == 0){
+            resultado = resultado + centena(value[i])
+        }
+        if(i == 1){
+            resultado = resultado + dezena(value[i])
+        }
+        if(i == 2){
+            resultado = resultado + unidade(value[i])
+        }
+    }
+    return resultado
 }
 
-function calculo (inputValue){
-    if(inputValue.slice(0, 1) == 9){
-        resultado = "Novecentos e ";
-        let resulDeze = dezen(inputValue, resultado);
-        
-        menorVinte(inputValue)
-       
-    }
-
-    if(inputValue.slice(0, 1) == 8){
-        resultado = "Oitocento e ";
-        let resulDeze = dezen(inputValue, resultado);
-        
-        if(inputValue.slice(1) < 20){
-
-            let final = espeCif(inputValue, resultado);
-            resultado = final
-            return resultado
-        }else{
-            let resultUni = unidade(inputValue, resulDeze)
-            resultado = resultUni 
-            return resultado
+function numeros(value){
+    let resultado = '';
+    for(let i = 0; i < value.length; i++){
+        if(i == 0){
+            resultado = resultado + dezena(value[i])
+        }
+        if(i == 1){
+            resultado = resultado + unidade(value[i])
         }
     }
-
-    if(inputValue.slice(0, 1) == 7){
-        resultado = "Setecentos e ";
-        let resulDeze = dezen(inputValue, resultado);
-        
-        if(inputValue.slice(1) < 20){
-
-            let final = espeCif(inputValue, resultado);
-            resultado = final
-            return resultado
-        }else{
-            let resultUni = unidade(inputValue, resulDeze)
-            resultado = resultUni 
-            return resultado
-        }
-    }
-
-    if(inputValue.slice(0, 1) == 6){
-        resultado = "Seissentos e ";
-        let resulDeze = dezen(inputValue, resultado);
-        
-        if(inputValue.slice(1) < 20){
-
-            let final = espeCif(inputValue, resultado);
-            resultado = final
-            return resultado
-        }else{
-            let resultUni = unidade(inputValue, resulDeze)
-            resultado = resultUni 
-            return resultado
-        }
-    }
-
-    if(inputValue.slice(0, 1) == 5){
-        resultado = "Quinhentos e ";
-        let resulDeze = dezen(inputValue, resultado);
-        
-        if(inputValue.slice(1) < 20){
-
-            let final = espeCif(inputValue, resultado);
-            resultado = final
-            return resultado
-        }else{
-            let resultUni = unidade(inputValue, resulDeze)
-            resultado = resultUni 
-            return resultado
-        }
-    }
-
-    if(inputValue.slice(0, 1) == 4){
-        resultado = "Quatrocentos e ";
-        let resulDeze = dezen(inputValue, resultado);
-        
-        if(inputValue.slice(1) < 20){
-
-            let final = espeCif(inputValue, resultado);
-            resultado = final
-            return resultado
-        }else{
-            let resultUni = unidade(inputValue, resulDeze)
-            resultado = resultUni 
-            return resultado
-        }
-    }
-
-    if(inputValue.slice(0, 1) == 3){
-        resultado = "Trezentos e ";
-        let resulDeze = dezen(inputValue, resultado);
-        
-        if(inputValue.slice(1) < 20){
-
-            let final = espeCif(inputValue, resultado);
-            resultado = final
-            return resultado
-        }else{
-            let resultUni = unidade(inputValue, resulDeze)
-            resultado = resultUni 
-            return resultado
-        }
-    }
-
-    if(inputValue.slice(0, 1) == 2){
-        resultado = "Duzentos e ";
-        let resulDeze = dezen(inputValue, resultado);
-        
-        if(inputValue.slice(1) < 20){
-
-            let final = espeCif(inputValue, resultado);
-            resultado = final
-            return resultado
-        }else{
-            let resultUni = unidade(inputValue, resulDeze)
-            resultado = resultUni 
-            return resultado
-        }
-    }
-
-    if(inputValue.slice(0, 1) == 1){
-        resultado = "Cento e ";
-        let resulDeze = dezen(inputValue, resultado);
-        
-        if(inputValue.slice(1) < 20){
-
-            let final = espeCif(inputValue, resultado);
-            resultado = final
-            return resultado
-        }else{
-            let resultUni = unidade(inputValue, resulDeze)
-            resultado = resultUni 
-            return resultado
-        }
-    }
+    return resultado
 }
 
+function centena(value){
+    let resultado
+    if(value == 9) resultado =' Novessentos e';  
+    if(value == 8) resultado =' Oitocentos e'; 
+    if(value == 7) resultado =' Setessentos e';
+    if(value == 6) resultado = " Seissentos e";   
+    if(value == 5) resultado =' Quinhentos e';   
+    if(value == 4) resultado =' Quatrocentos e';   
+    if(value == 3) resultado =' Trecentos e';    
+    if(value == 2) resultado =' Duzentos e';
+    if(value == 1) resultado =' Cento e';
+    return resultado;
+}
+
+function dezena(value){
+    let resultado = "";
+    if(value == 9) resultado =' noventa e';  
+    if(value == 8) resultado =' oitenta e'; 
+    if(value == 7) resultado =' setenta e';
+    if(value == 6) resultado = " sessenta e";   
+    if(value == 5) resultado =' cinquenta e';   
+    if(value == 4) resultado =' quarenta e';   
+    if(value == 3) resultado =' trinta e';    
+    if(value == 2) resultado =' vinte e';
+    return resultado;
+}
+
+function unidade(value){
+    let resultado = '';
+    if(value == 9) resultado =' nove';  
+    if(value == 8) resultado =' oito'; 
+    if(value == 7) resultado =' sete';
+    if(value == 6) resultado = " seis";   
+    if(value == 5) resultado =' cinco';   
+    if(value == 4) resultado =' quatro';   
+    if(value == 3) resultado =' tres';    
+    if(value == 2) resultado =' dois';
+    if(value == 1) resultado =' um';
+    if(value == 0) resultado =' zero';
+    return resultado;
+}
+
+function menorVinte(value){   
+    let resultado = '';
+    if(value == 19) resultado = ' desenove';
+    if(value == 18) resultado = ' dessoito';
+    if(value == 17) resultado = ' desessete';
+    if(value == 16) resultado = ' desesseis';  
+    if(value == 15) resultado = ' quinze'; 
+    if(value == 14) resultado = ' quatorze';
+    if(value == 13) resultado = ' treze';   
+    if(value == 12) resultado = ' doze';
+    if(value == 11) resultado = ' onze';
+    if(value == 10) resultado = 'dez'   
+    return resultado;
+}
 
 
 
